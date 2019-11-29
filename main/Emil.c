dbc98dd1f69c1cbdba7930cd8525c6cee8f17fdf
EmilGHenriksen <56338371+EmilGHenriksen@users.noreply.github.com>
2019-11-29 15:33:26 +0100

    Changes

--- main/Emil.c
+++ main/Emil.c
@@ -43,18 +43,19 @@ struct passenger {
 int main (void) {
     FILE *testFile;
     int ch;
+    int i = 0;
     passenger passengers[MAX_SEATS];
 
-    testFile = fopen("passengertest", "r");
-
+    testFile = fopen("passengertest.txt", "r");
 
     if (testFile != NULL) {
-        while ((ch = fgetc(testFile)) != EOF) {
-            while ((ch = fgetc(testFile)) != ' ') {
-                
-            }
+        while ((ch = fgetc(testFile)) != EOF && (ch = fgetc(testFile)) != 'A') {
+                passengers[i].seatPos.x = ch;
+                i++;
         }
     }
+// for testing purposes
+    printf("%lf\n%lf\n%lf\n%lf\n%lf\n", passengers[0].seatPos.x, passengers[1].seatPos.x, passengers[2].seatPos.x, passengers[3].seatPos.x, passengers[4].seatPos.x);
 
     return 0;
 }
