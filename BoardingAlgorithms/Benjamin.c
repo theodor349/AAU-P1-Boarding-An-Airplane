273793ccb9192c4088704ccdb648f71c05c15f0c
LuciferLied <Benjamin_porsmose@hotmail.com>
2019-11-29 14:13:53 +0100

    Updated Benjamin.c
    Copy pasted the general structure from Theodor.c and changed/added the assignments that i've undertaken.

--- BoardingAlgorithms/Benjamin.c
+++ BoardingAlgorithms/Benjamin.c
@@ -2,10 +2,127 @@
 //    steffen benjamin
 //    binary search benjamin
 
+//
+// Created by Theodor Risager on 29-11-2019.
+// Copied by Benjamin Porsmose on 29-11-2019
+
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
 
+typedef
+struct point {
+    float x, y;
+} point;
 
-int main (void){
+typedef
+enum boardingProcedure{
+    Random,
+    SteffenModified
+};
 
+typedef
+enum state {
+    LookingForRow = 0,
+    Luggage,
+    Seating,
+    Idle
+} state;
+
+typedef
+enum rowLetter{
+    Aisle = 0,
+    C, B, A,
+    D, E, F
+} rowLetter;
+
+typedef
+struct passenger {
+    point currPos;
+    point seatPos;
+    state currState;
+    int ticksToWait;
+    int hasLuggage;
+} passenger;
+
+void generatePassengers(passenger passengers[3]);
+
+void printPassenger(passenger passengers[3]);
+
+char GetSeatName(rowLetter letter);
+
+void steffen_que(passenger passengers[3], int numPassengers, enum boardingProcedure procedure);
+
+void binary_search();
+
+int main()
+{
+    passenger passengers[3];
+    generatePassengers(passengers);
+    printPassenger(passengers);
+
+    QueuePassengers(passengers, 3, Random);
+
+    return 0;
+}
+
+void steffen_que(passenger passengers[3], int numPassengers, enum boardingProcedure procedure)
+{
+    
+}
+
+void binary_search(){
+
+}
+
+/*
+ * Dummy stuff :D
+ */
+
+void printPassenger(passenger passengers[3])
+{
+    for (int i = 0; i < 3; ++i)
+    {
+        printf("%d row: %d, seat %c\n", i, (int) passengers[i].seatPos.x, GetSeatName((rowLetter) passengers[i].seatPos.y));
+    }
+}
+
+char GetSeatName(rowLetter letter)
+{
+    switch (letter)
+    {
+        case Aisle:
+            return 'N';
+        case C:
+            return 'C';
+        case B:
+            return 'B';
+        case A:
+            return 'A';
+        case D:
+            return 'D';
+        case E:
+            return 'E';
+        case F:
+            return 'F';
+    }
+}
+
+void generatePassengers(passenger passengers[3])
+{
+    passenger temp;
+    point p;
+
+    p.x = 1;
+    p.y = F;
+    temp.seatPos = p;
+    passengers[0] = temp;
+
+    p.x = 3;
+    p.y = A;
+    temp.seatPos = p;
+    passengers[1] = temp;
+
+    p.x = 3;
+    p.y = F;
+    temp.seatPos = p;
+    passengers[2] = temp;
+}
