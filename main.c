32a2d75afbf59749153ec835ea550971bcba5fb2
Gajhede <Hrgajhede@gmail.com>
2019-12-02 18:11:54 +0100

    init new main

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "Headers/BoardingAlgorithms.h"
+#include "Headers/Gather.h"
+#include "Headers/Simulation.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void){
+
+
+
+    return EXIT_SUCCESS;
+}
