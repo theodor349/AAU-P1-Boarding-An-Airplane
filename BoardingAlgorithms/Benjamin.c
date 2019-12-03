f79ec2fb670faf7a69eefb33af83969d06dcfb1c
Theodor Risager <theodor349@gmail.com>
2019-12-03 08:54:03 +0100

    Read Benjamin Code

--- BoardingAlgorithms/Benjamin.c
+++ BoardingAlgorithms/Benjamin.c
@@ -53,7 +53,7 @@ void QueuePassengers(passenger passengers[3], int numPassengers, enum boardingPr
 int passengerCompare(passenger* a, passenger* b);
 void random_Que(passenger *passengers, passenger *sortedPassengers, int numPassengers);
 void steffen_que(passenger *passengers, passenger *sorted_passengers, int numPassengers);
-int binary_search(passenger *sorted_passengers, int seat, int row, int num_passengers);
+passenger binary_search(passenger *sorted_passengers, int seat, int row, int num_passengers);
 
 
 int getSpot(int *takenSpots, int numPassengers);
@@ -66,7 +66,7 @@ int main()
     generatePassengers(passengers);
     printPassenger(passengers);
 
-    QueuePassengers(passengers, 3, Random);
+    QueuePassengers(passengers, 3, SteffenModified);
 
     printf("\n");
     printPassenger(passengers);
@@ -112,41 +112,40 @@ void random_Que(passenger *passengers, passenger *sortedPassengers, int numPasse
     }
 }
 
-void steffen_que(passenger *passengers, passenger *sorted_passengers, int numPassengers){
-    passenger* passengersCopy = calloc(numPassengers, sizeof(passengers[0]));
+void steffen_Que(passenger *passengers, passenger *sorted_passengers, int numPassengers)
+{
     int group = 0, row  = 0, seat = 0, check, place_in_que = 0, ps_nr = 0;
 
-    for ( group = 0; group < 4; group++){
-        for (row = (33 - (group/2)); row > 0 ; row-2){
+    for ( group = 0; group < 4; group++)
+    {
+        for (row = (33 - (group/2)); row > 0 ; row-2)
+        {
             check = 0;
-            for (seat = A ; seat > check; seat--){
-                if (group % 2 != 0){
+            for (seat = A ; seat > check; seat--)
+            {
+                if (group % 2 != 0)
+                {
                     seat += 3;
                     check += 3;
                 }
-                ps_nr = binary_search(sorted_passengers, seat, row ,numPassengers);
-                passengers[0] = sorted_passengers[ps_nr];
-
+                passengers[0] = binary_search(sorted_passengers, seat, row ,numPassengers);
             }
-            
         }
-        
     }
-    
 }
 
-int binary_search(passenger *sorted_passengers, int seat, int row, int num_passengers){
-    int result = 0, i, middle_nr_rows = 33/2, middle_nr_seats = 189/2, middle_num_passengers = num_passengers/2;
+passenger binary_search(passenger *sorted_passengers, int seat, int row, int num_passengers)
+{
+    int i, middle_nr_rows = 33/2, middle_nr_seats = 189/2, middle_num_passengers = num_passengers/2;
+    passenger result;
     if (sorted_passengers[middle_num_passengers].seatPos.x > row)
-        binary_search(sorted_passengers, seat, row, middle_num_passengers);
-
+        return binary_search(sorted_passengers, seat, row, middle_num_passengers);
     else if (sorted_passengers[middle_num_passengers].seatPos.x < row)
-        binary_search(sorted_passengers, seat, row, middle_num_passengers);
+        return binary_search(sorted_passengers, seat, row, middle_num_passengers);
+    else if (sorted_passengers[middle_num_passengers].seatPos.x == row)
+        result = sorted_passengers[middle_num_passengers];
 
-    else if (sorted_passengers[middle_num_passengers].seatPos.x == row){
-                result = sorted_passengers[middle_num_passengers].seatPos.x;
-    }
-    result = sorted_passengers;
+    result = sorted_passengers[middle_num_passengers];
 
     return result;
 }
@@ -178,12 +177,6 @@ int passengerCompare(passenger* a, passenger* b)
 /*
  * Dummy stuff :D
  */
-
-void steffen_Que(passenger *pPassenger, passenger *pPassenger1, int passengers)
-{
-
-}
-
 
 void printPassenger(passenger passengers[3])
 {
