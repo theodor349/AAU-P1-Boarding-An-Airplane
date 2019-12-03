fa8a2bd00361a43f892c15cd21063e80b11cc881
Gajhede <Hrgajhede@gmail.com>
2019-12-03 08:43:24 +0100

    fixed simulation definition dublications

--- Simulation/simulation.c
+++ Simulation/simulation.c
@@ -16,37 +16,6 @@
 #define MAX_ROWS 33
 #define MAX_SEATS 189
 
-typedef
-struct point {
-    float x, y;
-} point;
-
-typedef
-enum state {
-    LookingForRow = 0,
-    Luggage,
-    Seating,
-    Idle
-} state;
-
-typedef
-struct passenger {
-    point currPos;
-    point seatPos;
-    state currState;
-    int ticksToWait;
-    bool hasLuggage;
-} passenger;
-
-float runSimulation(passenger *pArr, int n);
-void updatePassenger(passenger *pArr, int i);
-void stateLookingForRow(passenger *pArr, int i);
-void stateLuggage(passenger *pArr, int i);
-void stateSeating(passenger *pArr, int i);
-int getPassengerAhead(passenger *pArr, int pIndex);
-float getTimestep(void);
-double gcd(double a, double b);
-
 // This is here temporarily so we can compile the file and see if there are any errors
 // compile with:     -std=c99 -pedantic -Wall
 int main(void) {
