36a10ba4de8a20aeb6ad4bf11159988bfd0346da
adamstuck <56338480+adamstuck@users.noreply.github.com>
2019-12-04 11:13:11 +0100

    Update LookingForRow.c

--- Simulation/States/LookingForRow.c
+++ Simulation/States/LookingForRow.c
@@ -8,6 +8,7 @@ void stateLookingForRow(passenger *pArr, int i) {
     if(infront == -1) {
         pArr[i].currPos.x += STEP_DISTANCE;
     } else {
+        // If the distance between two passengers is >= 2 Steps, the passenger behind can step forward
         if(pArr[infront].currPos.x - pArr[i].currPos.x >= STEP_DISTANCE * 2) {
             pArr[i].currPos.x += STEP_DISTANCE;
         }
