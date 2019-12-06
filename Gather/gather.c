66c75a91903aec833ebc64d6d06dbd3a6f1a5ddb
Theodor Risager <theodor349@gmail.com>
2019-12-06 11:11:35 +0100

    Commented Gather

--- Gather/gather.c
+++ Gather/gather.c
@@ -3,13 +3,17 @@
 #define MAX_SEATS 189
 #define LENGTH 10
 
+// Returns a passenger array 
 passenger* gather(FILE *passengerSource) {
     passenger *passengerArray;
     int passengerAmount = 0;
 
+    // Get number of passengers
     passengerAmount = getPassengerAmount(passengerSource);
 
+    // Create passengers array with space to all passengers
     passengerArray = calloc(passengerAmount, sizeof(passenger));
+    // Read the file and insert passengers
     readFile(passengerArray, passengerSource, passengerAmount);
 
     // printf("x = %d luggage(0, 1) = %d %d\n", passengerArray[10].seatPos.x, passengerArray[10].hasLuggage, passengerArray[10].seatPos.y);
@@ -31,6 +35,7 @@ int getPassengerAmount(FILE *passengerSource) {
     return passengerAmount;
 }
 
+// Reads a file and inputs all information about passengers into a passenger array
 void readFile(passenger *passengerArray, FILE *passengerSource, int passengerAmount) {
     char tempChar = 0;
     int i = 0;
@@ -39,13 +44,16 @@ void readFile(passenger *passengerArray, FILE *passengerSource, int passengerAmo
 
     if (passengerSource != NULL) {
         for (i = 0; i < passengerAmount; i++) {
+            // Format one line of the file 
             fscanf(passengerSource, " %d %c %d", &passengerArray[i].seatPos.x, &tempChar, &passengerArray[i].hasLuggage);
 
+            // Initialize passengers with zero values
             passengerArray[i].currPos.x = -1 - i;
             passengerArray[i].currPos.y = 0;
             passengerArray[i].currState = LookingForRow;
             passengerArray[i].ticksToWait = 0;
 
+            // Set Seat letter
             switch (tempChar) {
                 case 'A':
                     passengerArray[i].seatPos.y = A;
