a7ead30ce26153351907c4ff69d46eed5c7f4528
adamstuck <56338480+adamstuck@users.noreply.github.com>
2019-12-09 09:03:42 +0100

    Pynter op kommentarer

--- Simulation/States/Luggage.c
+++ Simulation/States/Luggage.c
@@ -3,14 +3,14 @@
 #define LUGGAGE_STORE_TICKS 3
 
 void stateLuggage(passenger *pArr, int i) {
-	// If the passengers has no luggage or has stowed it away
+	/* If the passengers has no luggage or has stowed it away */
     if(pArr[i].hasLuggage == STOWED_AWAY_LUGGAGE || pArr[i].hasLuggage == NO_LUGGAGE) {
         pArr[i].currState = Seating;
         return;
     }
 
-    // Set the passenger has luggage to stowed away
+    /* Set the passenger has luggage to stowed away */
     pArr[i].hasLuggage = STOWED_AWAY_LUGGAGE;
-    // Set time to wait
+    /* Set time to wait */
     pArr[i].ticksToWait = LUGGAGE_STORE_TICKS;
 }
--- Simulation/States/Seating.c
+++ Simulation/States/Seating.c
@@ -1,32 +1,32 @@
 #include "..\..\Header.h"
 
 void stateSeating(passenger *pArr, int pArrSize, int i) {
-	// Is the passenger at the intended seat?
+	/* Is the passenger at the intended seat? */
 	if(pArr[i].currPos.y == pArr[i].seatPos.y) {
-		// Then set the passenger to Idle
+		/* Then set the passenger to Idle */
 		pArr[i].currState = Idle;
 		return;
 	}
 
-	// Get number of passengers blocking the way to the seat
+	/* Get number of passengers blocking the way to the seat */
 	int O = countPassengersInRow(pArr, pArrSize, i);
 	if(O > 0) {
-		// Get amount of seated passengers on this row
+		/* Get amount of seated passengers on this row */
 		int Sp = abs(pArr[getClosestToAisle(pArr, pArrSize, i)].seatPos.y);
-		// Set time to wait
+		/* Set time to wait */
 		pArr[i].ticksToWait = 2*(O + Sp) - (Sp - 1);
 	}
-	// Set the passenger to be at the intented seat
+	/* Set the passenger to be at the intented seat */
     pArr[i].currPos.y = pArr[i].seatPos.y;
 }
 
 int countPassengersInRow(passenger *pArr, int pArrSize, int pI) {
 	int n = 0;
-	// For each passenger 
+	/* For each passenger */
 	for(int i = 0; i < pArrSize; i++) {
-		// If the passenger is idle(seated) and at the correct row
+		/* If the passenger is idle(seated) and at the correct row */
 		if(pArr[i].currState == Idle && pArr[i].currPos.x == pArr[pI].seatPos.x) {
-			// If the seated passenger is blocking the way (Closer to the aisle than the destination seat)
+			/* If the seated passenger is blocking the way (Closer to the aisle than the destination seat) */
 			if((pArr[pI].seatPos.y > 0 && pArr[i].seatPos.y > 0) ||
 			   (pArr[pI].seatPos.y < 0 && pArr[i].seatPos.y < 0))
                 n++;
@@ -38,18 +38,18 @@ int countPassengersInRow(passenger *pArr, int pArrSize, int pI) {
 int getClosestToAisle(passenger *pArr, int pArrSize, int pI) {
 	int closestI, dist = 999;
 
-	// For each passenger
+	/* For each passenger */
 	for(int i = 0; i < pArrSize; i++) {
-		// If the passenger is idle(seated) and at the correct row
+		/* If the passenger is idle(seated) and at the correct row */
 		if(pArr[i].currState == Idle && pArr[i].currPos.x == pArr[pI].seatPos.x) {
-			// If the seated passenger is blocking the way (Closer to the aisle than the destination seat)
+			/* If the seated passenger is blocking the way (Closer to the aisle than the destination seat) */
 			if((pArr[pI].seatPos.y > 0 && pArr[i].seatPos.y > 0) ||
 			   (pArr[pI].seatPos.y < 0 && pArr[i].seatPos.y < 0)) {
-			   	// Is the distance to the passenger less than the current distance?
+			   	/* Is the distance to the passenger less than the current distance? */
 				if(abs(pArr[i].seatPos.y) < dist) {
-					// Set dist
+					/* Set dist */
 					dist = abs(pArr[i].seatPos.y);
-					// Set closest Passenger 
+					/* Set closest Passenger */
 					closestI = i;
 				}
 			}
